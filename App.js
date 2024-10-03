0e11793fa22f594d5795404369f710c6f571dd42
Taher Ben sedrine <114356513+taherbns@users.noreply.github.com>
2024-10-03 11:37:25 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
-
 function App() {
   const [file, setFile] = useState(null); 
   const [summary, setSummary] = useState(''); 
+  const [loading, setLoading] = useState(false); // État pour le chargement
 
   const handleFileUpload = (event) => {
     setFile(event.target.files[0]);
@@ -16,6 +16,8 @@ function App() {
       const formData = new FormData();
       formData.append('file', file);
   
+      setLoading(true); // Commencer le chargement
+  
       try {
         const response = await axios.post('http://localhost:5000/upload', formData, {
           headers: {
@@ -23,11 +25,12 @@ function App() {
           },
         });
   
-        console.log("Réponse reçue : ", response.data);  // Log pour vérifier la réponse
-  
         setSummary(response.data.summary);
       } catch (error) {
         console.error('Erreur lors de l\'envoi du fichier:', error);
+        setSummary("Une erreur est survenue lors de la génération du résumé.");
+      } finally {
+        setLoading(false); // Fin du chargement
       }
     } else {
       alert('Veuillez importer un fichier !');
@@ -46,6 +49,8 @@ function App() {
         <div className="summary-section">
           <button onClick={handleGenerateSummary} className="btn btn-primary">Générer un Résumé</button>
         </div>
+
+        {loading && <p>Chargement...</p>} {/* Indicateur de chargement */}
 
         {summary && (
           <div className="summary-result">
